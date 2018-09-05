3d7761c2735ddbb37880c251d02ad674df637a80
buttars <buttars0070@gmail.com>
2018-09-05 16:31:34 -0600

    Dynamic form app initialization

---
+++ dynamic-forms/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { QuestionService } from './question.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [QuestionService]
+})
+export class AppComponent {
+  title = 'dynamic-forms';
+
+  questions: any[];
+
+  constructor(service: QuestionService) {
+    this.questions = service.getQuestions();
+  }
+}
